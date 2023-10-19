fbda3b01dcc71c3c97862846b46d67a3f30b2109
anagotoyas <ana20otoya@gmail.com>
2023-10-19 05:45:14 -0500

    deploy on render

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 const client = axios.create({
     // baseURL: 'https://fyn6neyc0i.execute-api.us-east-1.amazonaws.com/api',
-    baseURL: 'http://localhost:3000/api',
+    // baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://voist-backend.onrender.com/api',
+
     withCredentials:true
 })
 
