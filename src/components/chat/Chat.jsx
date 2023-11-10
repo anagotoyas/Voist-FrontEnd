3d619cfb8687c6fb01140b593b8f1f652b3617f0
anagotoyas <ana20otoya@gmail.com>
2023-11-09 23:31:46 -0500

    add three dots loading in messages

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -5,6 +5,7 @@ import { Message } from "./message/Message";
 import { useAuth } from "../../context/AuthContext";
 import { RiSendPlaneFill } from "react-icons/ri";
 import { Spin } from "antd";
+import { ThreeDots } from "react-loader-spinner";
 
 export const Chat = (props) => {
   const { classId, url_pdf } = props;
@@ -15,22 +16,20 @@ export const Chat = (props) => {
 
   const [isLoadingRequest, setIsLoadingRequest] = useState(false);
 
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   const messagesContainerRef = useRef(null);
 
   const refetchConversations = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
       const data = await loadConversation(classId);
       setConversations(data);
-    
     } catch (error) {
       // Manejar el error si es necesario
       toast.warning(getErrorResponse(error));
-    }
-    finally{
-      setLoading(false)
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -49,7 +48,7 @@ export const Chat = (props) => {
       messagesContainerRef.current.scrollTop =
         messagesContainerRef.current.scrollHeight;
     }
-  }, [conversations]); 
+  }, [conversations]);
 
   const handleChangeInput = (e) => {
     setQuestion(e.target.value);
@@ -67,16 +66,14 @@ export const Chat = (props) => {
     try {
       setQuestion("");
       const response = await askQuestions(url_pdf, question);
-      console.log(response);
 
       const response2 = await createConversations(
         classId,
         question,
         response.answer
       );
-      console.log(response2);
+
       setConversations([...conversations, response2]);
-      console.log(conversations);
     } catch (error) {
       toast.warning(getErrorResponse(error));
     } finally {
@@ -98,66 +95,79 @@ export const Chat = (props) => {
   return (
     <div className="mt-10 border-gray-300 border-2 rounded-lg p-4 flex flex-col gap-8 justify-start lg:w-[70%] md:w-[60%] ">
       {loading ? (
-        <Spin/>
-      ):(
-      <>
-      <div
-        ref={messagesContainerRef}
-        className="pr-5 overflow-y-auto overflow-x-hidden min-h-[60vh] max-h-[60vh]"
-      >
-        {conversations.map((item, index) => (
-          <>
-            <Message
-              key={item.id}
-              content={item.question}
-              created_at={item.created_at}
-              isUserMessage
+        <Spin />
+      ) : (
+        <>
+          <div
+            ref={messagesContainerRef}
+            className="pr-5 overflow-y-auto overflow-x-hidden min-h-[60vh] max-h-[60vh]"
+          >
+            {conversations.map((item, index) => (
+              <>
+                <Message
+                  key={item.id}
+                  content={item.question}
+                  created_at={item.created_at}
+                  isUserMessage
+                />
+                <Message
+                  key={index}
+                  content={item.answer}
+                  created_at={item.created_at}
+                  isUserMessage={false}
+                />
+              </>
+            ))}
+            {isLoadingRequest ? (
+              <>
+                <Message
+                  content=""
+                  createdAt=""
+                  isUserMessage
+                  className="relative"
+                >
+                  <ThreeDots
+                    height="20"
+                    width="50"
+                    color="#fff"
+                    ariaLabel="three-dots-loading"
+                  />
+                </Message>
+                <Message
+                  content=""
+                  createdAt=""
+                  isUserMessage={false}
+                >
+                    <ThreeDots
+                    height="20"
+                    width="50"
+                    color="#777"
+                    ariaLabel="three-dots-loading"
+                  />
+                </Message>
+              </>
+            ) : null}
+          </div>
+          <div className="flex relative items-center ">
+            <input
+              type="text"
+              onKeyUp={handleKeyBoardInput}
+              value={question}
+              onChange={handleChangeInput}
+              className="rounded-xl border-2 border-gray-300 p-2 w-full"
             />
-            <Message
-              key={index}
-              content={item.answer}
-              created_at={item.created_at}
-              isUserMessage={false}
+            <RiSendPlaneFill
+              onClick={() => {
+                if (isLoadingRequest) return;
+                if (question !== "") {
+                  createConversation(question);
+                }
+              }}
+              className="w-[2rem] text-primary text-[1.2rem] absolute m-auto top-[.8rem] right-4 cursor-pointer hover:text-grey"
             />
-          </>
-        ))}
-        {isLoadingRequest ? (
-          <>
-            <Message
-              className="animate-pulse"
-              content=""
-              createdAt=""
-              isUserMessage
-            />
-            <Message
-              className="animate-pulse"
-              content=""
-              createdAt=""
-              isUserMessage={false}
-            />
-          </>
-        ) : null}
-      </div>
-      <div className="flex relative items-center ">
-        <input
-          type="text"
-          onKeyUp={handleKeyBoardInput}
-          value={question}
-          onChange={handleChangeInput}
-          className="rounded-xl border-2 border-gray-300 p-2 w-full"
-        />
-        <RiSendPlaneFill
-          onClick={() => {
-            if (isLoadingRequest) return;
-            if (question !== "" ) {
-              createConversation(question);
-            }
-          }}
-          className="w-[2rem] text-primary text-[1.2rem] absolute m-auto top-[.8rem] right-4 cursor-pointer hover:text-grey"
-        />
-      </div>
-      </>)}
-      
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/components/chat/message/Message.jsx
+++ src/components/chat/message/Message.jsx
@@ -3,7 +3,7 @@ import moment from "moment";
 import PropTypes from 'prop-types'
 
 export const Message = (props) => {
-  const { content, created_at, isUserMessage, className = "" } = props;
+  const { content, created_at, isUserMessage, className = "", children } = props;
 
   return (
     <div
@@ -11,9 +11,11 @@ export const Message = (props) => {
       className={`flex flex-col gap-2 ${isUserMessage ? "items-end" : "items-start"} ${className}}`}
 
     >
+
       <div
       className={`relative max-w-xl ${isUserMessage ? "bg-primary" : "bg-slate-200"} rounded-lg py-2 px-4 shadow-md`}
       >
+        {children}
         <span
         className={`text-sm text-white w-[1rem] h-[1rem] ${isUserMessage ? "bg-blue-500 rounded-br-full top-0 -right-2 text-white" : "bg-grey rounded -bl-full top-0 -left-3"} `}
         >
@@ -41,6 +43,7 @@ Message.propTypes = {
     created_at: PropTypes.string.isRequired,
     isUserMessage: PropTypes.bool.isRequired,
     className: PropTypes.string,
+    children: PropTypes.any
 
 
     };
