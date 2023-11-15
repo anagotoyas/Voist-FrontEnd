83124edd9761b672d638a717fb85cb3b55f859a9
anagotoyas <ana20otoya@gmail.com>
2023-11-15 07:15:41 -0500

    setTimeout of websockets

--- src/components/websocket/WebsocketComponent.jsx
+++ src/components/websocket/WebsocketComponent.jsx
@@ -9,36 +9,46 @@ export const WebsocketComponent = (props) => {
   const { user } = useAuth();
 
   useEffect(() => {
-    if(user && user.id){
-      const socket = io(DOMAIN, {
-      auth: {
-        token: user,
-        user: user,
-      },
-
-    });
-
-    // Evento cuando la conexión se establece
-    socket.on("connect", () => {
-      console.log("Conexión establecida con el servidor WebSocket");
-    });
-
-    // Evento cuando el usuario se desconecta
-    socket.on("disconnect", () => {
-      console.log("Desconexión del servidor WebSocket");
-    });
-
-    return () => {
-      // Desconectar el socket cuando el componente se desmonta
-      socket.disconnect();
-    };
-    }else{
-      console.log('nada')
-      return
+    let socket;
+  
+    if (user && user.id) {
+      // Esperar 2 segundos antes de intentar establecer la conexión
+      const connectTimeout = setTimeout(() => {
+        // Establecer la conexión WebSocket
+        socket = io(DOMAIN, {
+          auth: {
+            token: user,
+            user: user,
+          },
+        });
+  
+        // Evento cuando la conexión se establece
+        socket.on("connect", () => {
+          console.log("Conexión establecida con el servidor WebSocket");
+        });
+  
+        // Evento cuando el usuario se desconecta
+        socket.on("disconnect", () => {
+          console.log("Desconexión del servidor WebSocket");
+        });
+      }, 2000); // Esperar 2 segundos (2000 milisegundos)
+  
+      // Limpieza del efecto
+      return () => {
+        // Limpiar el temporizador si el componente se desmonta antes de que se establezca la conexión
+        clearTimeout(connectTimeout);
+        
+        // Desconectar el socket si ya se estableció la conexión
+        if (socket && socket.connected) {
+          socket.disconnect();
+        }
+      };
+    } else {
+      console.log('nada');
+      return;
     }
-   
-
-  }, [user]); // Asegúrate de agregar user como una dependencia para que el efecto se ejecute cuando cambie el usuario
+  }, [user]);
+  
 
   return <>{props.children}</>;
 };
