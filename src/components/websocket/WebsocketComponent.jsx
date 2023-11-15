c4f27ad751f20c7aa344ef1a90ca8c02ec4abb65
anagotoyas <ana20otoya@gmail.com>
2023-11-15 07:18:01 -0500

    fix websockets

--- src/components/websocket/WebsocketComponent.jsx
+++ src/components/websocket/WebsocketComponent.jsx
@@ -3,56 +3,56 @@ import { io } from "socket.io-client";
 import { useEffect } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { DOMAIN } from "../../api/api";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 export const WebsocketComponent = (props) => {
   const { user } = useAuth();
 
   useEffect(() => {
     let socket;
-  
+
     if (user && user.id) {
       // Esperar 2 segundos antes de intentar establecer la conexión
-      const connectTimeout = setTimeout(() => {
-        // Establecer la conexión WebSocket
-        socket = io(DOMAIN, {
-          auth: {
-            token: user,
-            user: user,
-          },
-        });
-  
+
+      // Establecer la conexión WebSocket
+      socket = io(DOMAIN, {
+        auth: {
+          token: user,
+          user: user,
+        },
+      });
+
+      if (socket) {
         // Evento cuando la conexión se establece
         socket.on("connect", () => {
           console.log("Conexión establecida con el servidor WebSocket");
         });
-  
+
         // Evento cuando el usuario se desconecta
         socket.on("disconnect", () => {
-          console.log("Desconexión del servidor WebSocket");
+          console.log(
+            "Desconexión del servidor WebSocket" + user.id + socket.id
+          );
         });
-      }, 2000); // Esperar 2 segundos (2000 milisegundos)
-  
-      // Limpieza del efecto
+      }
+
       return () => {
-        // Limpiar el temporizador si el componente se desmonta antes de que se establezca la conexión
-        clearTimeout(connectTimeout);
-        
+       
+
         // Desconectar el socket si ya se estableció la conexión
         if (socket && socket.connected) {
           socket.disconnect();
         }
       };
     } else {
-      console.log('nada');
+      console.log("nada");
       return;
     }
   }, [user]);
-  
 
   return <>{props.children}</>;
 };
 
 WebsocketComponent.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
 };
